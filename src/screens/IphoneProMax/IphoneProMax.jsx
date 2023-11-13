6bf8142a809cbd48db2be18f4bcbc045073b93c4
juliobarbagallo <40072754+juliobarbagallo@users.noreply.github.com>
2023-11-13 11:18:31 -0300

    Initial commit

---
+++ src/screens/IphoneProMax/IphoneProMax.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./style.css";
+
+export const IphoneProMax = () => {
+  return (
+    <div className="iphone-pro-max">
+      <div className="overlap-wrapper">
+        <div className="overlap">
+          <img
+            className="invitation-figma"
+            alt="Invitation figma"
+            src="https://c.animaapp.com/GpchFkYV/img/invitation-figma-1.png"
+          />
+          <div className="button-parroquia">
+            <div className="overlap-group">
+              <div className="rectangle" />
+              <p className="text-wrapper">Parroquia inmaculada concepción: José cubas 3599</p>
+            </div>
+          </div>
+          <div className="overlap-group-wrapper">
+            <div className="div">
+              <div className="rectangle-2" />
+              <p className="p">Festejo en Tamarindo: campana 1747</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
