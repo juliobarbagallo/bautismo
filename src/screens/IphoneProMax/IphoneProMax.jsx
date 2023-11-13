349c5d78709a03e21392951b01c986e0c439520a
julio cesar barbagallo <julio.barbagallo@gmail.com>
2023-11-13 20:36:55 -0300

    feat: Adds titles

--- src/screens/IphoneProMax/IphoneProMax.jsx
+++ src/screens/IphoneProMax/IphoneProMax.jsx
@@ -13,9 +13,41 @@ export const IphoneProMax = () => {
     }
   }, []);
 
+
+  // const fireConfetti = () => {
+  //   confetti();
+
+  //   setTimeout(() => {
+  //     window.open('https://forms.gle/vqj751mpHtnXPscC9');
+  //   }, 1500);
+  // };
+
   const fireConfetti = () => {
-    confetti();
+    var duration = 15 * 1000;
+    var animationEnd = Date.now() + duration;
+    var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+    function randomInRange(min, max) {
+      return Math.random() * (max - min) + min;
+    }
+
+    var interval = setInterval(function() {
+      var timeLeft = animationEnd - Date.now();
+
+      if (timeLeft <= 0) {
+        return clearInterval(interval);
+      }
+
+      var particleCount = 50 * (timeLeft / duration);
+      confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } });
+      confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } });
+    }, 250);
+
+    setTimeout(() => {
+      window.open('https://forms.gle/vqj751mpHtnXPscC9');
+    }, 2000);
   };
+
 
   return (
     <div className="iphone-pro-max">
@@ -44,11 +76,11 @@ export const IphoneProMax = () => {
             </a>
           </div>
           <div className="asisitir-button">
-            <a href="https://forms.gle/vqj751mpHtnXPscC9" target="_blank" rel="noopener noreferrer">
+            {/* <a href="https://forms.gle/vqj751mpHtnXPscC9" target="_blank" rel="noopener noreferrer"> */}
               <div className="div-wrapper">
                 <div className="text-wrapper-2" onClick={fireConfetti}>Confirmar asistencia</div>
               </div>
-            </a>
+            {/* </a> */}
           </div>
         </div>
       </div>
