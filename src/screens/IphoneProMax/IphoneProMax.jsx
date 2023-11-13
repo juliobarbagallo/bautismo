514edf3a09bbd2546f94daffb833feaaf3eb44bf
julio cesar barbagallo <julio.barbagallo@gmail.com>
2023-11-13 12:26:22 -0300

    feat: Adds buttons

--- src/screens/IphoneProMax/IphoneProMax.jsx
+++ src/screens/IphoneProMax/IphoneProMax.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./style.css";
+import angieValen from "../../../static/Invitation-figma2.png";
 
 export const IphoneProMax = () => {
   return (
@@ -9,21 +10,36 @@ export const IphoneProMax = () => {
           <img
             className="invitation-figma"
             alt="Invitation figma"
-            src="https://c.animaapp.com/GpchFkYV/img/invitation-figma-1.png"
+            // src="https://c.animaapp.com/GpchFkYV/img/invitation-figma-1.png"
+            src={angieValen}
           />
           <div className="button-parroquia">
-            <div className="overlap-group">
-              <div className="rectangle" />
-              <p className="text-wrapper">Parroquia inmaculada concepción: José cubas 3599</p>
-            </div>
+            <a href="https://maps.app.goo.gl/HMLMjjS96yk1GvPb8" target="_blank" rel="noopener noreferrer">
+              <div className="overlap-group">
+                <div className="rectangle" />
+                <p className="text-wrapper">11:30 | Parroquia inmaculada concepción: José cubas 3599</p>
+              </div>
+            </a>
           </div>
           <div className="overlap-group-wrapper">
-            <div className="div">
-              <div className="rectangle-2" />
-              <p className="p">Festejo en Tamarindo: campana 1747</p>
-            </div>
+            <a href="https://maps.app.goo.gl/fDmbJ7k7h2hZT9iM9" target="_blank" rel="noopener noreferrer">
+              <div className="div">
+
+                <div className="rectangle-2" />
+                <p className="p">13:00 | Festejo en Tamarindo: campana 1747</p>
+              </div>
+            </a>
           </div>
-        </div>
+          <div className="asisitir-button">
+            <a href="https://forms.gle/vqj751mpHtnXPscC9" target="_blank" rel="noopener noreferrer">
+            <div className="div-wrapper">
+              <div className="text-wrapper-2">Confirmar asistencia</div>
+            </div>
+            </a>
+          </div>
+
+          
+        </div> 
       </div>
     </div>
   );
